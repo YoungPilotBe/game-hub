849063ee06175f53a9a98e53803b95131bc78d72
YoungPilotBe <youngpilotbe@gmail.com>
2023-06-04 23:27:31 +0200

    Skeleton Cards

---
+++ game-hub/src/components/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton
+        height="225px"
+        width="300px"
+        borderRadius={10}
+        overflow="hidden"
+      ></Skeleton>
+      <CardBody>
+        <SkeletonText></SkeletonText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
